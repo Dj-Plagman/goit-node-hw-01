1ad96e8c50e03819a1faf58d960553bc9a4e09bb
Serhii Tsytsora <novochervone@gmail.com>
2021-12-20 20:56:02 +0200

    Home work 1

---
+++ README.md
@@ -0,0 +1,11 @@
+# node_blendet
+
+Посилання на світлини результатів команд
+
+https://ibb.co/d2Jfrrj "node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22"
+
+https://ibb.co/8s0kVvR "node index.js --action get --id 5"
+
+https://ibb.co/V9s8hm1 "node index.js --action list"
+
+https://ibb.co/4WHhYDK "node index.js --action remove --id=3"
---
+++ index.js
@@ -0,0 +1,49 @@
+const { Command } = require('commander');
+
+const { listContacts, getContactById, removeContact, addContact } = require("./contacts")
+
+const program = new Command();
+
+program
+    .requiredOption('-a, --action <type>', 'choose action')
+    .option('-i, --id <type>', 'user id')
+    .option('-n, --name <type>', 'user name')
+    .option('-e, --email <type>', 'user email')
+    .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+
+    switch (action) {
+        case 'list':
+            const contacts = await listContacts()
+            console.table(contacts);
+            break;
+
+        case 'get':
+            const contactById = await getContactById(id)
+            if (contactById) {
+                console.log(contactById)
+                return
+            }
+            break;
+
+        case 'add':
+            const contact = await addContact(name, email, phone)
+            console.log(contact);
+            break;
+
+        case 'remove':
+            const newContacts = await removeContact(id)
+            console.table(newContacts);
+            break;
+
+        default:
+            console.warn(chalk.red('Unknown action type!'));
+    }
+}
+
+invokeAction(argv).then(() => console.log('operation success'));
